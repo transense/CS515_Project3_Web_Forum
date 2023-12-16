175f4639048a0412adabd2285e46a2474c5fe491
Vaibhavi.shah <vaibhavi.shah2804@gmail.com>
2023-12-16 16:17:39 -0500

    App file updated

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+import json
+import secrets
+import datetime
+import threading
+
+app = Flask(__name__)
+
+# In-memory storage for posts
+posts = {}
+lock = threading.Lock()
+
+def generate_key():
+    return secrets.token_urlsafe(32)
+
+def get_current_timestamp():
+    return datetime.datetime.utcnow().isoformat()
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    try:
+        data = request.get_json()
+        msg = data['msg']
+    except (json.JSONDecodeError, KeyError):
+        return jsonify({"err": "Bad Request"}), 400
+
+    with lock:
+        post_id = len(posts) + 1
+        key = generate_key()
+        timestamp = get_current_timestamp()
+
+        posts[post_id] = {
+            "id": post_id,
+            "key": key,
+            "timestamp": timestamp,
+            "msg": msg
+        }
+
+    return jsonify({
+        "id": post_id,
+        "key": key,
+        "timestamp": timestamp
+    })
+
+@app.route('/post/<int:post_id>', methods=['GET'])
+def read_post(post_id):
+    with lock:
+        if post_id not in posts:
+            return jsonify({"err": "Not Found"}), 404
+
+        post = posts[post_id]
+        return jsonify({
+            "id": post["id"],
+            "timestamp": post["timestamp"],
+            "msg": post["msg"]
+        })
+
+@app.route('/post/<int:post_id>/delete/<key>', methods=['DELETE'])
+def delete_post(post_id, key):
+    with lock:
+        if post_id not in posts:
+            return jsonify({"err": "Not Found"}), 404
+
+        post = posts[post_id]
+        if key != post["key"]:
+            return jsonify({"err": "Forbidden"}), 403
+
+        del posts[post_id]
+
+    return jsonify({
+        "id": post["id"],
+        "key": post["key"],
+        "timestamp": post["timestamp"]
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
