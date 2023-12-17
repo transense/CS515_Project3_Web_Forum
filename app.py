26b092ad476a59fb6a565466d848dd7d6e2aa2ec
Vaibhavi shah <48199124+transense@users.noreply.github.com>
2023-12-17 16:50:48 -0500

    Rectified create post error

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 posts = {}
 lock = threading.Lock()
 
+# Assuming you have a list of users defined somewhere
+users = [
+    {"id": 1, "username": "user1", "email": "user1@example.com"},
+    {"id": 2, "username": "user2", "email": "user2@example.com"}
+]
+
 @app.route('/test_form')
 def indexForm():
     return render_template('index.html')
@@ -20,24 +26,20 @@ def generate_key():
 
 def get_user_by_id(user_id):
     for user in users:
-        if user['id'] == user_id:
+        if user['id'] == user_id:   
             return user
     return None
 
-def get_post_by_id(post_id):
-    for post in posts:
-        if post['id'] == post_id:
-            return post
-    return None
-
 def get_current_timestamp():
-    return datetime.datetime.utcnow().isoformat()
+    return datetime.utcnow().isoformat()
 
 @app.route('/post', methods=['POST'])
 def create_post():
     try:
         data = request.get_json()
         msg = data['msg']
+        user_id = data['user_id']
+        parent_id = data.get('parent_id')
     except (json.JSONDecodeError, KeyError):
         return jsonify({"err": "Bad Request"}), 400
 
@@ -50,8 +52,15 @@ def create_post():
             "id": post_id,
             "key": key,
             "timestamp": timestamp,
-            "msg": msg
+            "msg": msg,
+            "user_id": user_id,
+            "parent_id": parent_id,
+            "replies": []
         }
+
+        # Update parent post with the current post ID as a reply
+        if parent_id in posts:
+            posts[parent_id]["replies"].append(post_id)
 
     return jsonify({
         "id": post_id,
@@ -108,48 +117,22 @@ def get_posts_by_user(user_id):
                 "id": post["id"],
                 "timestamp": post["timestamp"],
                 "msg": post["msg"],
-                "user": user 
+                "user": user
             }
             for post in user_posts
         ]
 
         return jsonify(formatted_posts), 200
 
-@app.route('/post', methods=['POST'])
-def create_post():
-    try:
-        data = request.get_json()
-        msg = data['msg']
-        user_id = data['user_id']  
-        parent_id = data.get('parent_id')  
-    except (json.JSONDecodeError, KeyError):
-        return jsonify({"err": "Bad Request"}), 400
-
+@app.route('/user', methods=['POST'])
+def create_user():
     with lock:
-        post_id = len(posts) + 1
-        key = generate_key()
-        timestamp = get_current_timestamp()
+        user_id = len(users) + 1
+        user_key = generate_key()
+        user = {'id': user_id, 'key': user_key}
+        users.append(user)
 
-        posts[post_id] = {
-            "id": post_id,
-            "key": key,
-            "timestamp": timestamp,
-            "msg": msg,
-            "user_id": user_id,
-            "parent_id": parent_id,
-            "replies": []  
-        }
-
-        # Update parent post with the current post ID as a reply
-        if parent_id in posts:
-            posts[parent_id]["replies"].append(post_id)
-
-    return jsonify({
-        "id": post_id,
-        "key": key,
-        "timestamp": timestamp
-    })
-
+    return jsonify(user)
 
 @app.route('/posts/date-range', methods=['GET'])
 def get_posts_by_date_range():
@@ -184,4 +167,5 @@ def get_posts_by_date_range():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
