cb6c197eddd392b8a2db209b001320323d95b401
Vaibhavi shah <48199124+transense@users.noreply.github.com>
2023-12-17 21:13:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,69 @@
 from flask import Flask, request, jsonify
-import json
+import uuid
+import datetime
 import secrets
-from datetime import datetime
-
 import threading
 
 app = Flask(__name__)
 
-# In-memory storage for posts
 posts = {}
+users = {}
 lock = threading.Lock()
 
-# Assuming you have a list of users defined somewhere
-users = [
-    {"id": 1, "username": "user1", "email": "user1@example.com"},
-    {"id": 2, "username": "user2", "email": "user2@example.com"}
-]
+@app.route('/user', methods=['POST'])
+def create_user():
+    try:
+        data = request.get_json()
+        user_id = len(users) + 1
+        user_key = secrets.token_hex(16)
+        unique_metadata = data['unique_metadata']
+        non_unique_metadata = data.get('non_unique_metadata', '')
 
-@app.route('/test_form')
-def indexForm():
-    return render_template('index.html')
+        with lock:
+            users[user_id] = {
+                'id': user_id,
+                'key': user_key,
+                'unique_metadata': unique_metadata,
+                'non_unique_metadata': non_unique_metadata
+            }
 
-def generate_key():
-    return secrets.token_urlsafe(32)
+        return jsonify({'id': user_id, 'key': user_key}), 201
 
-def get_user_by_id(user_id):
-    for user in users:
-        if user['id'] == user_id:   
-            return user
-    return None
+    except (KeyError, ValueError) as e:
+        return jsonify({'err': str(e)}), 400
 
-def get_current_timestamp():
-    return datetime.utcnow().isoformat()
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_metadata(user_id):
+    with lock:
+        if user_id not in users:
+            return jsonify({'err': 'User not found'}), 404
+
+        user = users[user_id]
+        return jsonify({'id': user['id'], 'unique_metadata': user['unique_metadata']}), 200
+
+@app.route('/user/<int:user_id>/edit', methods=['PUT'])
+def edit_user_metadata(user_id):
+    try:
+        data = request.get_json()
+        user_key = data['key']
+        new_metadata = data['new_metadata']
+
+        with lock:
+            if user_id not in users:
+                return jsonify({'err': 'User not found'}), 404
+
+            user = users[user_id]
+
+            if user_key != user['key']:
+                return jsonify({'err': 'Forbidden'}), 403
+
+            # Update user metadata
+            user['unique_metadata'] = new_metadata
+
+            return jsonify({'id': user['id'], 'unique_metadata': user['unique_metadata']}), 200
+
+    except (KeyError, ValueError) as e:
+        return jsonify({'err': str(e)}), 400
 
 @app.route('/post', methods=['POST'])
 def create_post():
@@ -39,132 +71,79 @@ def create_post():
         data = request.get_json()
         msg = data['msg']
         user_id = data['user_id']
-        parent_id = data.get('parent_id')
-    except (json.JSONDecodeError, KeyError):
-        return jsonify({"err": "Bad Request"}), 400
+        user_key = data['user_key']
+        reply_to = data.get('reply_to', None)
 
-    with lock:
-        post_id = len(posts) + 1
-        key = generate_key()
-        timestamp = get_current_timestamp()
+        with lock:
+            # Create unique ID and key
+            post_id = len(posts) + 1
+            post_key = secrets.token_hex(16)  # Generate a 32-character (16 bytes) random key
 
-        posts[post_id] = {
-            "id": post_id,
-            "key": key,
-            "timestamp": timestamp,
-            "msg": msg,
-            "user_id": user_id,
-            "parent_id": parent_id,
-            "replies": []
-        }
+            # Create timestamp
+            timestamp = datetime.datetime.utcnow().isoformat()
 
-        # Update parent post with the current post ID as a reply
-        if parent_id in posts:
-            posts[parent_id]["replies"].append(post_id)
+            # Create post
+            posts[post_id] = {
+                'id': post_id,
+                'key': post_key,
+                'timestamp': timestamp,
+                'msg': msg,
+                'user_id': user_id,
+                'reply_to': reply_to
+            }
 
-    return jsonify({
-        "id": post_id,
-        "key": key,
-        "timestamp": timestamp
-    })
+            # Return response
+            return jsonify({'id': post_id, 'key': post_key, 'timestamp': timestamp}), 201
+
+    except (KeyError, ValueError) as e:
+        return jsonify({'err': str(e)}), 400
 
 @app.route('/post/<int:post_id>', methods=['GET'])
 def read_post(post_id):
     with lock:
         if post_id not in posts:
-            return jsonify({"err": "Not Found"}), 404
+            return jsonify({'err': 'Post not found'}), 404
 
         post = posts[post_id]
-        return jsonify({
-            "id": post["id"],
-            "timestamp": post["timestamp"],
-            "msg": post["msg"]
-        })
 
-@app.route('/post/<int:post_id>/delete/<key>', methods=['DELETE'])
-def delete_post(post_id, key):
-    with lock:
-        if post_id not in posts:
-            return jsonify({"err": "Not Found"}), 404
+        # Include the id of the post to which it is replying
+        reply_to_id = post.get('reply_to', None)
+        reply_to_info = {'reply_to': reply_to_id} if reply_to_id else {}
 
-        post = posts[post_id]
-        if key != post["key"]:
-            return jsonify({"err": "Forbidden"}), 403
+        # Include the ids of every reply to that post
+        replies = [pid for pid, pdata in posts.items() if pdata.get('reply_to') == post_id]
+        replies_info = {'replies': replies} if replies else {}
 
-        del posts[post_id]
+        return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg'],
+                        'user_id': post['user_id'], **reply_to_info, **replies_info}), 200
 
-    return jsonify({
-        "id": post["id"],
-        "key": post["key"],
-        "timestamp": post["timestamp"]
-    })
-
-@app.route('/user/<int:user_id>/posts', methods=['GET'])
-def get_posts_by_user(user_id):
-    with lock:
-        user = get_user_by_id(user_id)
-
-        if user is None:
-            return jsonify({"err": "User Not Found"}), 404
-
-        user_posts = [post for post in posts.values() if post.get("user_id") == user_id]
-
-        if not user_posts:
-            return jsonify({"msg": "No posts found for the user"}), 200
-
-        formatted_posts = [
-            {
-                "id": post["id"],
-                "timestamp": post["timestamp"],
-                "msg": post["msg"],
-                "user": user
-            }
-            for post in user_posts
-        ]
-
-        return jsonify(formatted_posts), 200
-
-@app.route('/user', methods=['POST'])
-def create_user():
-    with lock:
-        user_id = len(users) + 1
-        user_key = generate_key()
-        user = {'id': user_id, 'key': user_key}
-        users.append(user)
-
-    return jsonify(user)
-
-@app.route('/posts/date-range', methods=['GET'])
-def get_posts_by_date_range():
+@app.route('/posts/search', methods=['GET'])
+def search_posts_by_datetime():
     try:
-        start_date_str = request.args.get('start_date')
-        end_date_str = request.args.get('end_date')
+        start_datetime = request.args.get('start_datetime')
+        end_datetime = request.args.get('end_datetime')
 
-        if start_date_str is None and end_date_str is None:
-            return jsonify({"err": "At least one of 'start_date' or 'end_date' is required."}), 400
+        with lock:
+            filtered_posts = []
 
-        start_date = datetime.strptime(start_date_str, "%Y-%m-%dT%H:%M:%S") if start_date_str else None
-        end_date = datetime.strptime(end_date_str, "%Y-%m-%dT%H:%M:%S") if end_date_str else None
+            for post_id, post_data in posts.items():
+                post_timestamp = datetime.datetime.fromisoformat(post_data['timestamp'])
+
+                if (start_datetime is None or post_timestamp >= datetime.datetime.fromisoformat(start_datetime)) and \
+                        (end_datetime is None or post_timestamp <= datetime.datetime.fromisoformat(end_datetime)):
+                    filtered_posts.append({
+                        'id': post_data['id'],
+                        'timestamp': post_data['timestamp'],
+                        'msg': post_data['msg'],
+                        'user_id': post_data['user_id']
+                    })
+
+            return jsonify(filtered_posts), 200
+
     except ValueError:
-        return jsonify({"err": "Invalid date/time format. Use ISO 8601 format (e.g., '2023-12-31T23:59:59')"}), 400
+        return jsonify({'err': 'Invalid datetime format'}), 400
 
-    with lock:
-        filtered_posts = []
 
-        for post in posts.values():
-            post_timestamp = datetime.strptime(post["timestamp"], "%Y-%m-%dT%H:%M:%S")
-
-            # Check if the post falls within the specified date range
-            if (start_date is None or post_timestamp >= start_date) and (end_date is None or post_timestamp <= end_date):
-                filtered_posts.append({
-                    "id": post["id"],
-                    "timestamp": post["timestamp"],
-                    "msg": post["msg"],
-                    "user": get_user_by_id(post["user_id"])  # Include user information
-                })
-
-        return jsonify(filtered_posts), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
-
