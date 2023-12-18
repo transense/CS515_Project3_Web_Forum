1feae2fde4c00509b39a4ce0922ebdb4c23213b1
N <150329215+nmscs@users.noreply.github.com>
2023-12-17 19:31:21 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display a message indicating the start of the setup process
+echo "Starting setup process..."
+
+# Install Flask and Django using pip
+pip install Flask Django
+
+# Install any additional dependencies you may need for your project
+# Example: pip install dependency1 dependency2
+
+# Display a message indicating the completion of the setup process
+echo "Setup process completed successfully."
